01c9efd7765970774bbd5e3b2950d906494097cc
Kryvtsun Alina <krivtsun1002@gmail.com>
2023-07-02 17:03:43 +0300

    save dataForm

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,15 @@
+const formEl = document.querySelector('.feedback-form');
+formEl.addEventListener('input', onHandlerInput);
+const LOCALSTORAGE_KEY = "feedback-form-state";
+const dataForm = {};
+function onHandlerInput(evt) {
+    evt.preventDefault();
+    dataForm[evt.target.name] = evt.target.value;
+    console.log(dataForm);
+    saveDataToLocalStorage();  
+}
+function saveDataToLocalStorage() {
+    const jsonData = JSON.stringify(dataForm);
+  localStorage.setItem(LOCALSTORAGE_KEY, jsonData);
+}
 
