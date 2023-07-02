e1ad777d25485ade9eeebae87fef1a6056d35d01
Kryvtsun Alina <krivtsun1002@gmail.com>
2023-07-02 17:52:35 +0300

    add load dataform

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,15 +1,46 @@
 const formEl = document.querySelector('.feedback-form');
 formEl.addEventListener('input', onHandlerInput);
+formEl.addEventListener('submit', onSubmitHandler);
 const LOCALSTORAGE_KEY = "feedback-form-state";
-const dataForm = {};
+let dataForm = {};
+
+loadDataFromLocalStorage();
+
 function onHandlerInput(evt) {
     evt.preventDefault();
     dataForm[evt.target.name] = evt.target.value;
-    console.log(dataForm);
     saveDataToLocalStorage();  
 }
+
 function saveDataToLocalStorage() {
     const jsonData = JSON.stringify(dataForm);
-  localStorage.setItem(LOCALSTORAGE_KEY, jsonData);
+    localStorage.setItem(LOCALSTORAGE_KEY, jsonData);
 }
 
+function loadDataFromLocalStorage() {
+    const savedData = localStorage.getItem(LOCALSTORAGE_KEY);
+    if (savedData) {
+        dataForm = JSON.parse(savedData);
+        fillFormFields();
+    }
+}
+
+function fillFormFields() {
+    for (let field in dataForm) {
+        const inputField = formEl.querySelector(`[name="${field}"]`);
+        if (inputField) {
+            inputField.value = dataForm[field];
+        }
+    }
+}
+
+function onSubmitHandler(event) {
+    event.preventDefault();
+    console.log(dataForm);
+    clearLocalStorage();
+    formEl.reset();
+}
+
+function clearLocalStorage() {
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+}
