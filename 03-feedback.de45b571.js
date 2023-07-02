1208761a01f6894d9913e9d0584073acd2087bfe
Kryvtsun-Alina <krivtsun1002@gmail.com>
2023-07-02 14:04:20 +0000

    Deploying to gh-pages from @ Kryvtsun-Alina/goit-js-hw-08@01c9efd7765970774bbd5e3b2950d906494097cc 🚀

---
+++ 03-feedback.de45b571.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector(".feedback-form").addEventListener("input",(function(a){a.preventDefault(),t[a.target.name]=a.target.value,console.log(t),n=JSON.stringify(t),localStorage.setItem(e,n);var n}));var e="feedback-form-state",t={}}();
+//# sourceMappingURL=03-feedback.de45b571.js.map
